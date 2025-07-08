6e6498eece7a1b16056025908fa10fc0a417382f
lzichi <laura.r.zichi@gmail.com>
2025-07-08 09:54:48 -0600

    remove print

--- lammps_plugins/pair_flare.cpp
+++ lammps_plugins/pair_flare.cpp
@@ -256,7 +256,7 @@ double PairFLARE::compute_atomic_energy(int i, NeighList *neighborList)
   // error out if uncertainty is too high; garbage prediction
   // this may be the first time the simulation sees this type of atom
   if(variance > 0.02) {
-    printf("High variance in compute_atomic_atom, type %d", atom->type[i]);
+    //printf("High variance in compute_atomic_atom, type %d", atom->type[i]);
     return -1000;
     //error->all(FLERR, "Too high of uncertainty!");
   }
